6e1e344d98ddf252537fdd593a9e93d05917728c
friformato9 <franco.riformato@nttdata.com>
2025-09-29 15:20:20 +0200

    feat(shell): route to `one` and `two`

--- workspaces/application-shell/src/app/app.routes.ts
+++ workspaces/application-shell/src/app/app.routes.ts
@@ -1,3 +1,30 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'one',
+    loadComponent: () =>
+      loadRemoteModule({
+        type: 'manifest',
+        remoteName: 'feature-one-app',
+        exposedModule: './ComponentOne',
+      }).then((m) => {
+        console.log('Loading', m);
+        return m.FeatureOneApp;
+      }),
+  },
+  {
+    path: 'two',
+    loadComponent: () =>
+      loadRemoteModule({
+        type: 'manifest',
+        remoteName: 'feature-two-app',
+        exposedModule: './ComponentTwo',
+      }).then((m) => m.FeatureTwoApp),
+  },
+  {
+    path: '**',
+    redirectTo: 'one',
+  },
+];
