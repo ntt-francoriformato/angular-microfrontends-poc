c551d623f9e73d34c5ed97bc91d801bb3cb15efe
friformato9 <franco.riformato@nttdata.com>
2025-09-29 15:46:39 +0200

    feat(one): configure `one` to avoid conflict

--- workspaces/feature-one-app/src/app/app.ts
+++ workspaces/feature-one-app/src/app/app.ts
@@ -2,11 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-one',
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class FeatureOneApp {
   protected readonly title = signal('feature-one-app');
 }
--- workspaces/feature-one-app/src/bootstrap.ts
+++ workspaces/feature-one-app/src/bootstrap.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { FeatureOneApp } from './app/app';
 
-bootstrapApplication(App, appConfig)
+bootstrapApplication(FeatureOneApp, appConfig)
   .catch((err) => console.error(err));
